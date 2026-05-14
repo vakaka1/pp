54ae440179bd118b6ddb73316a1992e6fbbdd09b
vakaka1 <valdis.ai.gpt@gmail.com>
2026-05-14 14:33:58 +0300

    feat: запускать pp-client скрытым при запросе прав администратора (SW_HIDE)

--- cmd/pp-client/elevate_windows.go
+++ cmd/pp-client/elevate_windows.go
@@ -53,7 +53,7 @@ func ensureAdmin(log *zap.Logger) bool {
 	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
 	argPtr, _ := syscall.UTF16PtrFromString(args)
 
-	var showCmd int32 = 1 // SW_NORMAL
+	var showCmd int32 = 0 // SW_HIDE
 
 	err = windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
 	if err != nil {
